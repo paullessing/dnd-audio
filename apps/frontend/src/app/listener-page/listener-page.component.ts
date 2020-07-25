982ba45bb314e13e77ad56ae6214f296b1ad4145
paullessing <paul@paullessing.com>
2020-07-25 21:28:31 +0100

    Peer.js implemented but not working

--- apps/frontend/src/app/listener-page/listener-page.component.ts
+++ apps/frontend/src/app/listener-page/listener-page.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import type Peer from 'peerjs';
 import { PeerService } from '../peer.service';
 
 @Component({
   selector: 'dnd-audio-listener-page',
-  template: `Connection ID: {{ id }}`,
+  template: `Connection ID: {{ id }}<br><video #video></video>`,
 })
 export class ListenerPageComponent implements OnInit {
 
   public id: string;
+
+  @ViewChild('video')
+  public videoEl: ElementRef<HTMLVideoElement>;
+
+  private peer: Peer;
 
   constructor(
     private peers: PeerService
@@ -22,9 +28,42 @@ export class ListenerPageComponent implements OnInit {
 
     peer.on('open', (id) => {
       this.id = id;
-
-      peer.connect('dnd-audio-server')
     });
+
+    peer.on('error', (err) => {
+      console.log('Peer error', err);
+    });
+
+    console.log('Connecting');
+    const conn = peer.connect('dnd-audio-server');
+    conn.on('open', () => {
+      console.log('Connection is open');
+    });
+    conn.on('error', (err) => {
+      console.log('Conn error', err);
+    });
+    peer.on('call', (call) => {
+      console.log('Getting a call');
+
+      call.answer();
+      call.on('stream', (mediaStream: MediaStream) => {
+        console.log('Got a stream');
+        this.videoEl.nativeElement.srcObject = mediaStream;
+      });
+    })
   }
 
+  public connect(): void {
+    const getUserMedia: typeof navigator.getUserMedia =
+      (navigator as any).getUserMedia ||
+      (navigator as any).webkitGetUserMedia ||
+      (navigator as any).mozGetUserMedia;
+
+    getUserMedia({video: true}, (stream) => {
+      const call = this.peer.call('another-peers-id', stream);
+    }, (err) => {
+      console.log('Failed to get local stream', err);
+    });
+
+  }
 }
--- apps/frontend/src/app/stream-page/stream-page.component.ts
+++ apps/frontend/src/app/stream-page/stream-page.component.ts
@@ -1,30 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import type Peer from 'peerjs';
 import { PeerService } from '../peer.service';
 
 @Component({
   selector: 'dnd-audio-stream-page',
-  template: `Status: {{ status }}`,
+  template: `Status: {{ status }}<br><button *ngIf="status === 'CONNECTED'" (click)="go()">Get Stream</button>`,
 })
 export class StreamPageComponent implements OnInit {
 
   public status: string;
+  private peer: Peer
+
+  private stream: Promise<MediaStream>;
+  private resolve: any;
 
   constructor(
     private peers: PeerService
   ) {
     this.status = 'NONE';
+
+    this.stream = new Promise((resolve) => this.resolve = resolve);
   }
 
   public ngOnInit(): void {
     console.log('Initialising');
-    const peer = this.peers.create('dnd-audio-server');
-    console.log('peer', peer);
+    this.peer = this.peers.create('dnd-audio-server');
+    console.log('peer', this.peer);
 
     this.status = 'CONNECTING';
 
-    peer.on('open', () => {
+    this.peer.on('open', () => {
       this.status = 'CONNECTED';
     });
+
+    this.peer.on('error', (err) => {
+      console.log('Peer error', err);
+    });
+
+    this.peer.on('connection', async (dataConnection) => {
+      const id = dataConnection.peer;
+      console.log('Got connection from', id);
+
+      const stream = await this.stream;
+      console.log('Calling', id);
+      const call = this.peer.call(id, stream); // Send my media
+      console.log('Called', call);
+
+      dataConnection.close();
+    });
   }
+
+  public go()
+{
+  const getUserMedia: typeof navigator.getUserMedia =
+    (navigator as any).getUserMedia ||
+    (navigator as any).webkitGetUserMedia ||
+    (navigator as any).mozGetUserMedia;
+
+  getUserMedia({ video: true },
+    (stream) => {
+      this.resolve(stream);
+      console.log('resolved', stream);
+    }, (err) => {
+    console.log('Failed to get local stream' , err);
+  });
+}
 
 }
